25caa743f5cfbbcc3f3ec0cdebe2074709a247fb
Lawrence Naumov <prostolawr@gmail.com>
2023-01-25 15:18:29 +0800

    Edit site

--- app/hole.js
+++ app/hole.js
@@ -1,16 +1,16 @@
 import three from "three";
 
 
-
 AFRAME.registerComponent('cloak', {
     init: function() {
-        let geometry = new THREE.BoxGeometry(1, 1, 1)
-        geometry.faces.splice(4, 2) // cut out the top faces
+        let geometry = new THREE.BoxGeometry(1, 1, 1);
+        geometry.faces.splice(4, 2); // cut out the top faces
         let material = new THREE.MeshBasicMaterial({
             colorWrite: false
         });
         let mesh = new THREE.Mesh(geometry, material);
-        // mesh.scale.set(1.1, 1.1, 1.1)
+        mesh.scale.set(1.1, 1.1, 1.1);
         this.el.object3D.add(mesh);
+        console.log("Hello!")
     }
 })
